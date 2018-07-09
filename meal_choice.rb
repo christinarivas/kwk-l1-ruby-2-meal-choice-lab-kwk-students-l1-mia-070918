da42304f0327a5a8b631ccc824d44d4fd11d0362
christinarivas <christina@rivastwins.com>
2018-07-09 19:58:59 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -5,7 +5,9 @@ end
 
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
 
-
+def breakfast(bfood)
+  puts "breakfast choice: #{bfood}"
+end
 
 
 
@@ -20,4 +22,5 @@ puts snacks("Swedish fish")
 
 # Call your methods without any arguments here
 puts snacks
+breakfast("sanwich")
 ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
